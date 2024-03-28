969bfb857d844c74a1cb4f0032b4a6d8dc804189
Christian Scarlett <christianscarlett21@gmail.com>
2024-03-28 00:15:03 -0400

    add send button

--- mobileapp/pages/Chat.tsx
+++ mobileapp/pages/Chat.tsx
@@ -1,12 +1,20 @@
 import {useHeaderHeight} from '@react-navigation/elements';
 import React, {useEffect, useState} from 'react';
-import {KeyboardAvoidingView, StyleSheet, TextInput, View} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import ChatMessages from '../components/Chat/ChatMessages';
 import PlayerStatuses from '../components/Chat/PlayerStatuses';
 import PuzzleInfoHeader from '../components/Chat/PuzzleInfoHeader';
 import useGameManager from '../lib/Game/useGameManager';
 import {Theme, useTheme} from '../lib/Theme';
 import {makeTextInputStyles} from '../styles/textInputStyle';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function Divider() {
   const [theme] = useTheme();
@@ -54,11 +62,16 @@ function Chat() {
           <PlayerStatuses playerStates={playerStates} />
           <Divider />
           <ChatMessages chatMessages={messages} />
-          <TextInput
-            style={makeTextInputStyles(theme).textInput}
-            placeholder="Say something..."
-            placeholderTextColor={theme.colors.mainGray1}
-          />
+          <View style={styles.bottomBar}>
+            <TextInput
+              style={makeTextInputStyles(theme).textInput}
+              placeholder="Say something..."
+              placeholderTextColor={theme.colors.mainGray1}
+            />
+            <TouchableOpacity style={styles.sendButton}>
+              <Icon name="send" style={styles.sendButtonIcon} />
+            </TouchableOpacity>
+          </View>
         </View>
       </KeyboardAvoidingView>
     </View>
@@ -67,9 +80,21 @@ function Chat() {
 
 const makeStyles = (theme: Theme) => {
   return StyleSheet.create({
-    input: {
+    bottomBar: {
       flexDirection: 'row',
       marginVertical: 5,
+    },
+    sendButton: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginHorizontal: 5,
+      padding: 10,
+      borderRadius: 5,
+      backgroundColor: theme.colors.primary,
+    },
+    sendButtonIcon: {
+      fontSize: 20,
+      color: theme.colors.background,
     },
     chat: {
       height: '100%',
--- mobileapp/styles/textInputStyle.tsx
+++ mobileapp/styles/textInputStyle.tsx
@@ -4,11 +4,11 @@ import {StyleSheet} from 'react-native';
 export const makeTextInputStyles = (theme: Theme) => {
   return StyleSheet.create({
     textInput: {
-      width: '100%',
+      flexGrow: 1,
       minHeight: 40,
       padding: 10,
       borderWidth: 1,
-      borderRadius: 10,
+      borderRadius: 5,
       borderColor: theme.colors.border,
     },
   });
